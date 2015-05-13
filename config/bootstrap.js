4e22a44a4748d106a6061ed32b0c621958a393ba
Marco F. Cavaliere <m.cavaliere@reply.it>
2015-05-13 09:59:03 +0200

    First Deploy Test

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -10,7 +10,42 @@
  */
 
 module.exports.bootstrap = function(cb) {
+	var mosca = require('mosca');
+	var pubsubsettings = {
+	  //using ascoltatore
+	  type: 'mongo',        
+	  url: 'mongodb://localhost:27017/mqtt',
+	  pubsubCollection: 'ascoltatori',
+	  mongo: {}
+	};
+	var moscaSettings = {
+	  port: 4000,               // mosca (mqtt) port: 1883
+	  backend: pubsubsettings   // pubsubsettings is the object we created above 
 
+	};
+
+	var server = new mosca.Server(moscaSettings);   // here we start mosca
+	server.on('ready', setup);                      // on init it fires up setup()
+
+	// fired when the mqtt server is ready
+	function setup() {
+	  sails.log.debug('Mosca server is up and running')
+	};
+	// fired when a message is published
+	server.on('published', function(packet, client) {
+	sails.log.info('Published', packet);
+	sails.log.info('Client', client);
+	});
+	// fired when a client connects
+	server.on('clientConnected', function(client) {
+	sails.log.info('Client Connected:', client.id);
+	});
+
+	// fired when a client disconnects
+	server.on('clientDisconnected', function(client) {
+	sails.log.info('Client Disconnected:', client.id);
+	});
+	
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
   cb();
